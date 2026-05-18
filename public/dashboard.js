1aaadbb9dde5ad5b55782ae57a722a6c3ebe14e5
xlegendario <dario.b1996@gmail.com>
2026-05-18 08:24:52 +0200

    Update dashboard.js

--- public/dashboard.js
+++ public/dashboard.js
@@ -312,7 +312,21 @@ function renderWtbOpenOffersRows(offers) {
             type="button"
             data-delete-offer-id="${escapeHtml(offer.id)}"
           >
-            🗑
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="18"
+              height="18"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2.6"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path d="M3 6h18"/>
+              <path d="M8 6V4h8v2"/>
+              <path d="M19 6l-1 14H6L5 6"/>
+            </svg>
           </button>
         </div>
       </td>
