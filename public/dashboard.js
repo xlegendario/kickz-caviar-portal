bbcfdf1fca96d36152bb4c79cc16a5b67034ad30
xlegendario <dario.b1996@gmail.com>
2026-05-18 12:48:07 +0200

    Update dashboard.js

--- public/dashboard.js
+++ public/dashboard.js
@@ -431,7 +431,7 @@ function renderReadyToShipRows(items) {
                   rel="noopener"
                   title="Download label"
                 >
-                  ↓
+                  ⬇
                 </a>
               `
               : ""
