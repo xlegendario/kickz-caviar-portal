29d2481416b4b30cecf35c8b0372825909556122
xlegendario <dario.b1996@gmail.com>
2026-05-18 11:45:27 +0200

    Update dashboard.js

--- public/dashboard.js
+++ public/dashboard.js
@@ -499,7 +499,8 @@ async function loadDashboardData() {
     `;
   
     const params = new URLSearchParams({
-      seller_record_id: dashboardSeller.id
+      seller_record_id: dashboardSeller.id,
+      seller_id: dashboardSeller.seller_id
     });
   
     const response = await fetch(
