79cb65e1af4352ab6922a4c07c9504d997cc9df3
xlegendario <dario.b1996@gmail.com>
2026-06-26 00:17:31 +0200

    Update dashboard.js

--- public/dashboard.js
+++ public/dashboard.js
@@ -1510,7 +1510,7 @@ function renderBuyingDeliveredRows(items) {
       <td>
         ${
           item.requires_payment
-            ? `<span class="dashboard-status-pill dashboard-status-payment">Payment Required</span>`
+            ? `<span class="dashboard-status-pill dashboard-status-payment">Not Paid</span>`
             : `<span class="dashboard-status-pill dashboard-status-open">Delivered</span>`
         }
       </td>
